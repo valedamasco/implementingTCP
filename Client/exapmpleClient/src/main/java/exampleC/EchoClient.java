b576216a5546f3a9e5d389afcfaa6ed3645d372d
Vale Damasco <vdamasco@localhost.localdomain>
2019-08-23 11:27:02 -0300

    last fix, cant send and recive from same socket

--- Client/exapmpleClient/src/main/java/exampleC/EchoClient.java
+++ Client/exapmpleClient/src/main/java/exampleC/EchoClient.java
@@ -14,42 +14,40 @@ import javax.swing.Timer;
 
 public class EchoClient {
 
+	
 	private static boolean ackOk = false;
 	private static InetAddress ip = null;
-
-	private static DatagramPacket DpSend = null;
-	private static DatagramSocket ds = null;
-	private static byte buf[] = null;
-	private static byte mes[] = null;
-	private static byte seq[] = null;
-
-		
-	private static DatagramSocket respon = null;
-	private static DatagramPacket DpReceive = null;
-	private static byte[] receive = new byte[65535];
-
 	private static int count = 0;
 
+	//Socket to be use, pck to send and other to recive 
+	private static DatagramSocket ds = null;
+	private static DatagramSocket dr = null;
+	private static DatagramPacket DpSend = null;
+	private static DatagramPacket DpRecive = null;
+
+	//Var to have the info to send and recive 
+	private static byte buf[] = null;
+	private static byte mes[] = null;
+	private static byte[] receive = new byte[65535];
+	//Var to use as de seq number before de message 
+	private static byte seq[] = null;
+	
 	public static void main(String args[]) throws IOException {
 		Scanner sc = new Scanner(System.in);
 
-		// Step 1:Create the socket object for
-		// carrying the data.
+		// Step 1:Create the socket object to carry the info 
 		ds = new DatagramSocket();
+		dr = new DatagramSocket(2345);
 		ip = InetAddress.getLocalHost();
-		
-
-		// rdt2.0
-
-		// message recive
-
-		respon = new DatagramSocket(2345);
-		
+  
+				
 		while (true) {
+			//ds.connect(ip, 1234);
+			//count how many message i sended 
 			count = 0;
-			
-			String inp = sc.nextLine();
 
+			//To read line 
+			String inp = sc.nextLine();
 			// convert the String input into the byte array.
 			buf = inp.getBytes();
 
@@ -58,23 +56,20 @@ public class EchoClient {
 
 			int seqLen = seq.length;
 			int bufLen = buf.length;
+			//Create de message with the seq number in the first two bytes
 			mes = new byte[seqLen + bufLen];
 			System.arraycopy(seq, 0, mes, 0, seqLen);
 			System.arraycopy(buf, 0, mes, seqLen, bufLen);
 
-			// Step 2 : Create the datagramPacket for sending the data.
-			// rdt1.0
+			// Step 2 : Create the datagramPacket for sending the data. rdt1.0
 			DpSend = new DatagramPacket(mes, mes.length, ip, 1234);
 			ds.send(DpSend);
 
-			if (inp.equals("bye")) {
-				//ds.close();
-				break;
-			}
 
-
-			Timer timer = new Timer(3000, new ActionListener() {
+			//Timer do actions when is certain time 	
+			Timer timer = new Timer(1000, new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
+					//If there's timeout and I didn's send the message 3 times, resend it 
 					if ((!ackOk) && (count < 3)){
 						try {
 							System.out.println("Did't recive the ACK, sending again");
@@ -89,40 +84,73 @@ public class EchoClient {
 			});
 			timer.start();
 
+
+			//If input is "bye", close de connection
+			if (inp.equals("bye"))
+				break;
+			
+			//Create a thread to wait the ACK
 			Thread t = new Thread(new reciveClass());
 			t.start();
 
-
+			// Clear the buffer after every message.
+			receive = new byte[65535];
 			
 
+
+			// // isBound() method 
+            // System.out.println("IsBound : " + ds.isBound()); 
+    
+            // // isConnected() method 
+            // System.out.println("isConnected : " + ds.isConnected()); 
+    
+            // // getInetAddress() method 
+            // System.out.println("InetAddress : " + ds.getInetAddress()); 
+    
+            // // getPort() method 
+            // System.out.println("Port : " + ds.getPort()); 
+    
+            // // getRemoteSocketAddress() method 
+            // System.out.println("Remote socket address : " +  
+            // ds.getRemoteSocketAddress()); 
+    
+            // // getLocalSocketAddress() method 
+            // System.out.println("Local socket address : " +  
+            // ds.getLocalSocketAddress()); 
+			
 		}
 	}
 
 	private static class reciveClass implements Runnable {
 
-
-
 		public void run() {	
 			try {			
 				// rdt2.0
-				DpReceive = new DatagramPacket(receive, receive.length);
-				respon.receive(DpReceive);
+				ackOk = false;
+
+				//Create de pck to send the ACK
+				DpRecive = new DatagramPacket(receive, receive.length);
+				dr.receive(DpRecive);
+
+				//Read the seq number in the ACK to compare with the one i sended 
 				int first = receive[0];
 				int second = receive[1];
 				ackOk = (first==seq[0]) && (second==seq[1]);
-				if (ackOk) {
-					System.out.println("Message was recive.");
-				}
-				receive = new byte[65535];
-
 				
+				if (ackOk) {
+					//They recive the message right 
+					System.out.println("Message was recive.");
+				}			
+			
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
+
 		}
 	}
 
+	//Function to generate seq number 
 	public static byte[] findSeq(byte[] seq) {
 		byte[] ret = seq;
 		if (ret == null) {
--- Server/exampleServer/src/main/java/exampleS/EchoServer.java
+++ Server/exampleServer/src/main/java/exampleS/EchoServer.java
@@ -4,62 +4,108 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 // Java program to illustrate Server side 
 // Implementation using DatagramSocket 
-import java.io.IOException;
-import java.net.DatagramPacket;
+import java.io.IOException; 
+import java.net.DatagramPacket; 
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.Timer;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class EchoServer {
+    
+    //Socket with two pck, one to recive and other so send 
     private static DatagramSocket ds = null;
-    private static byte[] seqIn = null;
+    private static DatagramPacket DsRecive = null;
+    private static DatagramPacket DsSend = null;
+    //private static byte[] seqIn = new byte[2];
+
+    private static InetAddress ip=null;
+    private static byte[] receive = new byte[65535];
+    private static Queue<int[]> seqQueue = null;
     public static void main(String[] args) throws IOException, InterruptedException {
+        
         // Step 1 : Create a socket to listen at port 1234
         ds = new DatagramSocket(1234);
-        byte[] receive = new byte[65535];
-        DatagramPacket DpReceive = null;
-
-        // rdt2.0 create the ACK package
+        //init the var         
+        ip = InetAddress.getLocalHost();
+        seqQueue = new LinkedList<>(); 
         
-        //DatagramSocket ackMessage = new DatagramSocket();
-        InetAddress ip = InetAddress.getLocalHost();
-
         while (true) {
+            
 
-            // Step 2 : create a DatgramPacket to receive the data.
-            // rdt1.0
-            DpReceive = new DatagramPacket(receive, receive.length);
-            // Step 3 : revieve the data in byte buffer.
-            ds.receive(DpReceive);
-            System.out.println("Lo que recivo es: " +receive[0] + " "+ receive[1]);
+            //Create var to se seq that we will have
+            byte[] seqIn = new byte[2];
+            //Create var to save in queue to know if i already recive the msg
+            int[] newData = new int[2];
 
-            // rdt2.0
-            seqIn = new byte[2];
+            // Create a DatgramPacket to receive the data. rdt1.0
+            DsRecive = new DatagramPacket(receive, receive.length);
+            //Revieve the data in byte buffer.
+            ds.receive(DsRecive);
+
+            // rdt2.0 Save the seq from the msg recive
             seqIn[0] = receive[0];
             seqIn[1] = receive[1];
+            newData[0] = (int)receive[0];
+            newData[1] = (int)receive[1];
+                        
+            //TimeUnit.SECONDS.sleep(4);
 
+            //Create pck to send the ACK withe the seq recived 
+            DsSend = new DatagramPacket(seqIn, seqIn.length, ip , 2345);
+             //if (seqQueue.size()>2) {
+                 //Send the pck ACK
+            ds.send(DsSend);
+             //}
+                                    
+            
+            //Check if i already recive the seq, so the msg will be ignor
+            if (seqQueue.isEmpty() || !isThere(newData[0],newData[1])) {
+                //System.out.println("Send it de seq" + newData[0]+ newData[1]);
+                //Display the msg without the seq
+                System.out.println("Client:-" + data(receive));
+                //Save the seq in queue
+                seqQueue.add(newData);
+            } else {
+                //Ignoring the msg cause is repeat
+                //System.out.println("Element is repeated.");
+            }
 
-
-
-            System.out.println("Client:-" + data(receive));
-
+            //When msg comming is "bye" will close the connection
             if (data(receive).toString().equals("bye")) {
                 System.out.println("Client sent bye.....EXITING");
-                ds.close();
+                ds.close();                
                 break;
             }
 
-            TimeUnit.SECONDS.sleep(10);
-  
-            DatagramPacket ackSend = new DatagramPacket(seqIn, seqIn.length, ip, 2345);
-            ds.send(ackSend);
-
-
-
             // Clear the buffer after every message.
             receive = new byte[65535];
+
+
+
+        	// // isBound() method 
+            // System.out.println("IsBound : " + ds.isBound()); 
+    
+            // // isConnected() method 
+            // System.out.println("isConnected : " + ds.isConnected()); 
+    
+            // // getInetAddress() method 
+            // System.out.println("InetAddress : " + ds.getInetAddress()); 
+    
+            // // getPort() method 
+            // System.out.println("Port : " + ds.getPort()); 
+    
+            // // getRemoteSocketAddress() method 
+            // System.out.println("Remote socket address : " +  
+            // ds.getRemoteSocketAddress()); 
+    
+            // // getLocalSocketAddress() method 
+            // System.out.println("Local socket address : " +  
+            // ds.getLocalSocketAddress()); 
         }
     }
 
@@ -69,12 +115,26 @@ public class EchoServer {
         if (a == null)
             return null;
         StringBuilder ret = new StringBuilder();
+        //Will ignor the 2 first bytes cause they are the seq
         int i = 2;
         while (i < a.length && a[i] != 0) {
             ret.append((char) a[i]);
             i++;
         }
         return ret;
+    }
+
+    //Checking in my queue that the seq was not safe before
+    public static boolean isThere(int first, int second){
+        boolean isThere = false ;
+        Iterator<int[]> iter = seqQueue.iterator();
+        while (iter.hasNext() && !isThere) {
+            int[] aux = iter.next();
+            if (aux[0]==first && aux[1]==second) {
+                isThere = true;
+            }             
+        }
+        return isThere;
     }
 
 }
