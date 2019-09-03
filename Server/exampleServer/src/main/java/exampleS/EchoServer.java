900f25f82a2fe1f661a73a8bf1e9959c68538d6b
Vale Damasco <valedamasco@Vales-MacBook-Air.local>
2019-09-03 10:02:48 -0300

    last update test Server

--- Server/exampleServer/src/main/java/exampleS/EchoServer.java
+++ Server/exampleServer/src/main/java/exampleS/EchoServer.java
@@ -17,16 +17,9 @@ import java.util.Queue;
 import java.util.concurrent.TimeUnit;
 
 public class EchoServer {
-    
-    //Socket with two pck, one to recive and other so send 
-//    private static DatagramSocket ds = null;
-//    private static DatagramPacket DsRecive = null;
-//    private static DatagramPacket DsSend = null;
-      private static byte[] seqIn = new byte[2];
 
-//    private static InetAddress ip=null;
-//    private static byte[] receive = new byte[65535];
-//    private static Queue<int[]> seqQueue = null;
+    private static byte[] seqIn = new byte[2];
+
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
@@ -35,13 +28,15 @@ public class EchoServer {
         InetAddress ip = InetAddress.getLocalHost();
         Queue<int[]> seqQueue = new LinkedList<>();
         DatagramPacket DsSend = null;
-        byte[] receive = new byte[65535];
-        DatagramPacket DsRecive = new DatagramPacket(receive, receive.length);
+
+
         
         while (true) {
+            byte[] receive = new byte[65535];
             String message = "";
-            message = waitMsg(ip, ds ,  seqQueue, receive , DsRecive );
-            sendAck(ds , DsSend,  ip );
+            message = waitMsg(ip, ds ,  seqQueue, receive  );
+            System.out.println(message);
+            sendAck(ds , DsSend,  ip, seqIn );
 
             if (message=="bye") {
                 System.out.println("Client sent bye.....EXITING");
@@ -56,7 +51,7 @@ public class EchoServer {
         return seqIn;
     }
 
-    public static byte[] sendAck(DatagramSocket ds , DatagramPacket DsSend, InetAddress ip ) throws IOException {
+    public static byte[] sendAck(DatagramSocket ds , DatagramPacket DsSend, InetAddress ip, byte[] seqIn ) throws IOException {
         //Create pck to send the ACK withe the seq recived
         DsSend = new DatagramPacket(seqIn, seqIn.length, ip , 2345);
         //if (seqQueue.size()>2) {
@@ -67,10 +62,10 @@ public class EchoServer {
     }
 
 
-    public static String waitMsg( InetAddress ip, DatagramSocket ds, Queue<int[]> seqQueue, byte[] receive,DatagramPacket DsRecive) throws IOException {
+    public static String waitMsg( InetAddress ip, DatagramSocket ds, Queue<int[]> seqQueue, byte[] receive) throws IOException {
 
         // Create a DatgramPacket to receive the data. rdt1.0
-
+        DatagramPacket DsRecive = new DatagramPacket(receive, receive.length);
         //Revieve the data in byte buffer.
         ds.receive(DsRecive);
 
@@ -96,13 +91,13 @@ public class EchoServer {
             //System.out.println("Send it de seq" + newData[0]+ newData[1]);
             //Display the msg without the seq
             msgRecive ="Client:-" + data(receive) ;
-            System.out.println("Client:-" + data(receive));
+            //System.out.println("Client:-" + data(receive));
             //Save the seq in queue
             seqQueue.add(newData);
         } else {
             //Ignoring the msg cause is repeat
             msgRecive = "Element is repeated." ;
-            System.out.println("Element is repeated.");
+            //System.out.println("Element is repeated.");
         }
 
         //When msg comming is "bye" will close the connection
@@ -110,7 +105,7 @@ public class EchoServer {
             return "bye";
         }
 
-        receive = new byte[65535];
+        //receive = new byte[65535];
         return msgRecive;
     }
 
--- Server/exampleServer/src/test/java/exampleS/EchoServerTest.java
+++ Server/exampleServer/src/test/java/exampleS/EchoServerTest.java
@@ -23,6 +23,7 @@ public class EchoServerTest {
     private static DatagramPacket DsSend;
     private static byte[] seqIn = new byte[2];
     private static EchoServer myServer ;
+    byte[] recive = new byte[6890];
 
     @BeforeClass
     public static void onceExecutedBeforeAll() throws IOException {
@@ -30,28 +31,64 @@ public class EchoServerTest {
         ds = new DatagramSocket(1234);
         ip = InetAddress.getLocalHost();
         seqQueue = new LinkedList<>();
+
     }
 
     @Test
     public void getMsg() throws IOException {
-        String msgToRecive = "My new msg";
-        byte[] buf = new byte[666];
-        byte[] seq = new byte[2];
-        seq[0] = 0;
-        seq[1] = 1;
-        int seqLen = 2;
-        int bufLen = msgToRecive.getBytes().length;
-        byte[] mes = new byte[seqLen + bufLen];
-        System.arraycopy(seq, 0, mes, 0, seqLen);
-        System.arraycopy(msgToRecive.getBytes(), 0, mes, seqLen, bufLen);
+        String msgExpected  = "Client:-My new msg";
+        Thread myMsgThread = new Thread(new CreateMsg());
+        myMsgThread.start();
+        String actualMsg = myServer.waitMsg( ip,  ds,  seqQueue, recive);
+        assertEquals(msgExpected,actualMsg);
+    }
 
-        DatagramPacket sendMsg = new DatagramPacket(mes, mes.length);
-        myServer.waitMsg( ip,  ds,  seqQueue, buf, sendMsg);
+    @Test
+    public void sendAck() throws IOException {
+        byte[] seqExpected = {0,1};
+        int seqE = seqExpected[0] + seqExpected[1];
+        Thread myMsgThread = new Thread(new CreateMsg());
+        myMsgThread.start();
+        String actualMsg = myServer.waitMsg( ip,  ds,  seqQueue, recive);
+        byte[] seqReal = myServer.getSeqIn();
+        int seqR = seqReal[0] + seqReal[1];
+
+        assertEquals(seqE,seqR);
+    }
+
+    @Test
+    public void elemRepeated() throws IOException {
+        String expected = "Element is repeated.";
+        Thread myMsgThread1 = new Thread(new CreateMsg());
+        myMsgThread1.start();
+        String actual = myServer.waitMsg( ip,  ds,  seqQueue, recive);
+        assertEquals(expected,actual);
+    }
 
 
-        System.out.println("llego a ejecutar algo ");
 
-        assertTrue(true);
+    public static class CreateMsg implements Runnable {
+        public void run() {
+            while (true) {
+                String msgToRecive = "My new msg";
+                byte[] buf = new byte[6660];
+                byte[] seq = new byte[2];
+                seq[0] = 0;
+                seq[1] = 1;
+                int seqLen = 2;
+                int bufLen = msgToRecive.getBytes().length;
+                byte[] mes = new byte[seqLen + bufLen];
+                System.arraycopy(seq, 0, mes, 0, seqLen);
+                System.arraycopy(msgToRecive.getBytes(), 0, mes, seqLen, bufLen);
+                try {
+                    DatagramPacket myMsg = new DatagramPacket(mes, mes.length, ip, 1234);
+                    ds.send(myMsg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
 
 }
